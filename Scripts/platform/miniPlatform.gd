5e3c93a609106f3ba3901131c34e1697cfcd533f
Vinícius <vinicius.victor.girotto@gmail.com>
2021-04-04 14:16:34 -0300

    espinhos flutuantes

--- Scripts/platform/miniPlatform.gd
+++ Scripts/platform/miniPlatform.gd
@@ -10,6 +10,8 @@ export var invisible = false
 export var moveH = false
 export var moveV = false
 export var rotate = false
+export var spikes = false
+export var upSpike = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +19,26 @@ func _ready():
 	if invisible:
 		$Timer.start(time)
 		enable = true
+	if spikes:
+		if $SmallSpikes:
+			$SmallSpikes.visible = true
+			$SmallSpikes/CollisionShape2D.disabled = false
+			if upSpike:
+				$SmallSpikes.position = Vector2(0, -9.822)
+				$SmallSpikes.rotation = 0
+			else:
+				$SmallSpikes.position = Vector2(0, 9.822)
+				$SmallSpikes.rotation = 180
+		else:
+			print("existe")
+			$LargeSpikes.visible = true
+			$LargeSpikes/CollisionShape2D.disabled = false
+			if upSpike:
+				$LargeSpikes.position = Vector2(0, -9.822)
+				$LargeSpikes.rotation = 0
+			else:
+				$LargeSpikes.position = Vector2(0, 9.822)
+				$LargeSpikes.rotation = 180
 
 func _on_Timer_timeout():
 	if enable:
